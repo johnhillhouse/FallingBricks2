f56f4a1de17523b0c66b58713fd248cdc6089824
johnhillhouse <johnhillhouse@gmail.com>
2013-07-31 17:33:57 +0100

    Trying to get view working from example

---
+++ FallingBricks2/View/GameView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FallingBricks2.Model.UI;
+
+namespace FallingBricks2.View
+{
+    /// <summary>
+    /// Interaction logic for GameView.xaml
+    /// </summary>
+    public partial class GameView : OverlayUserControl
+    {
+        public GameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
